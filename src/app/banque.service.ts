6d58709757ace3cde3912a0816d5637afb0ec430
hialmar <torguet@gmail.com>
2022-04-01 15:13:51 +0200

    Changed to heroku URL.

--- src/app/banque.service.ts
+++ src/app/banque.service.ts
@@ -4,30 +4,28 @@ import { InfoCompte } from './info.compte';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BanqueService {
+  currentId = '0';
 
-  currentId = "0";
+  restURL = 'https://hialmar-banque-node.herokuapp.com/compte/';
 
-  restURL = "http://localhost:3000/compte/";
-
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   creerCompte(ic: InfoCompte): Observable<any> {
     return this.httpClient.post(this.restURL, ic);
   }
 
-  ajouterAuCompte(ic: InfoCompte) : Observable<any> {
+  ajouterAuCompte(ic: InfoCompte): Observable<any> {
     return this.httpClient.put(this.restURL + ic._id, ic);
   }
 
-  retirerDuCompte(ic: InfoCompte) : Observable<any> {
+  retirerDuCompte(ic: InfoCompte): Observable<any> {
     return this.httpClient.put(this.restURL + ic._id, ic);
   }
 
   positionDuCompte(id: string): Observable<any> {
     return this.httpClient.get(this.restURL + id);
   }
-
 }
